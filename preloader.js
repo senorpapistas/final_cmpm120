bb6d4c074c97d41cb231e93859ca326fc9400b8b
Ewin <emathew@ucsc.edu>
2023-06-12 12:21:40 -0700

    added explosion to preloader

--- preloader.js
+++ preloader.js
@@ -12,8 +12,16 @@ class Preloader extends Phaser.Scene {
         this.load.image('space', 'Spacebackground.png');
         this.load.audio('boost', 'boost.wav');
         this.load.audio('bgm', 'creamy tomato.mp3');
-        this.load.image('planet1', 'planet1.png')
-        this.load.image('planet2','planet2.png')
+        this.load.image('planet1', 'planet1.png');
+        this.load.image('planet2','planet2.png');
+
+        this.load.image('megumin1', 'megumin/megumin1.png');
+        this.load.image('megumin2', 'megumin/megumin2.png');
+        this.load.image('megumin3', 'megumin/megumin3.png');
+        this.load.image('megumin4', 'megumin/megumin4.png');
+        this.load.image('megumin5', 'megumin/megumin5.png');
+        this.load.image('megumin6', 'megumin/megumin6.png');
+        this.load.image('megumin7', 'megumin/megumin7.png');
     }
     create() {
         this.scene.start('selectScreen');
