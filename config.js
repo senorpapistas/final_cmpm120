f051aec5438331326952130808c619998e81da8f
puargamer <ijkwok@ucsc.edu>
2023-06-12 11:56:32 -0700

    linking

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ let game = new Phaser.Game({
     },
     captions: false,
     type: Phaser.WEBGL,
-    scene: [Level1, Pause, TransitionScreen, Death],
+    scene: [selectScreen, cutscene, titleScreen, Level1, Pause, TransitionScreen, Death],
     title: 'Hotline Albuquerque',
 });
 game.config.captions = false;
