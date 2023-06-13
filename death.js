4195a978330131b7c16649af37c89f72d9dd01a2
puargamer <ijkwok@ucsc.edu>
2023-06-12 20:48:48 -0700

    qw

--- death.js
+++ death.js
@@ -12,6 +12,7 @@ class Death extends Phaser.Scene {
         this.add.text(game.config.width*.5, game.config.height*.2, 'YOU DIED', {fontSize: 120, color: '#ff0000'}).setOrigin(.5);
         this.add.text(game.config.width*.5, game.config.height*.3, "Click to restart", {fontSize: 40}).setOrigin(.5);
         this.input.on('pointerdown', () => {
+            losesfx.stop()
             this.scene.start('level1');
         });
     };
